973ed04720eab3f38a8737a7cebbff5e9ce1177d
Rick <67919881+BuRNoceros@users.noreply.github.com>
2021-09-30 17:50:26 -0400

    add name to user schema

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,13 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
+  name  : {
+    type      : String,
+    required  : true,
+    minlength : 3,
+    maxlength : 30
+  },
+
   email : {
     type     : String,
     required : true,
