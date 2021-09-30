215119c5065e10605cc8ca6487c7ca00350e5439
Rick <67919881+BuRNoceros@users.noreply.github.com>
2021-09-30 19:07:38 -0400

    add password to schema

--- server/models/user.js
+++ server/models/user.js
@@ -2,17 +2,23 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-  name  : {
+  name     : {
     type      : String,
     required  : true,
     minlength : 3,
     maxlength : 30
   },
 
-  email : {
+  email    : {
     type     : String,
     required : true,
     unique   : true
+  },
+  password : {
+    type      : String,
+    required  : true,
+    minlength : 6,
+    maxlength : 1024
   }
 })
 
