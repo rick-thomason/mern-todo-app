bae247109a4877b074e8aede18ad570a017f72b4
Rick <67919881+BuRNoceros@users.noreply.github.com>
2021-09-30 19:18:19 -0400

    change loginUser from .then to async await

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,7 +2,7 @@ const User = require('../models/user')
 
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body
-  const user = new User({ name, email, password })
+  let user = new User({ name, email, password })
   try {
     user = await user.save()
     res.status(201).send({ user })
@@ -11,19 +11,20 @@ const registerUser = async (req, res) => {
   }
 }
 
-const loginUser = (req, res) => {
+const loginUser = async (req, res) => {
   const { email, password } = req.body
-  User.findOne({ email })
-    .then(user => {
-      if (!user) {
-        return res.send('user not found')
-      }
-      if (user.password !== password) {
-        return res.send('wrong password')
-      }
-      res.send('user logged in')
-    })
-    .catch(err => res.json(err))
+  try {
+    const user = await User.findOne({ email })
+    if (!user) {
+      return res.status(404).send('user not found')
+    }
+    if (user.password !== password) {
+      return res.status(401).send('wrong password')
+    }
+    res.send('user logged in')
+  } catch (err) {
+    res.status(400).send(err.message)
+  }
 }
 
 const renderRegister = (req, res) => {
